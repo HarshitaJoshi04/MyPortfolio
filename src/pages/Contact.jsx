e28c2d1496c338064efd697a0459c6fc8568b9dc
HarshitaJoshi04 <hjoshikspuk04@gmail.com>
2026-02-21 00:47:30 +0530

    updated contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,36 +1,59 @@
 import { useContext, useState } from "react";
-
+import emailjs from "@emailjs/browser";
 import { Theme } from "../components/themecontext/ThemeContext";
 import H1 from "../components/headingAnimation/H1";
 
 const Contact = () => {
   const [text, setText] = useState("");
   const [email, setEmail] = useState("");
-
   const [message, setMessage] = useState("");
-  const submitHandle = (e) => {
-    e.preventDefault();
-    setText("");
-    setEmail("");
-    setMessage("");
-  };
 
   const [theme] = useContext(Theme);
   const isDark = theme === "Dark";
+
+  const submitHandle = (e) => {
+    e.preventDefault();
+
+    if (!text || !email || !message) {
+      alert("Please fill all fields");
+      return;
+    }
+
+    const templateParams = {
+      from_name: text,
+      from_email: email,
+      message: message,
+    };
+
+    emailjs
+      .send(
+        "service_mcospxx",
+        "template_zcgl4ql",
+        templateParams,
+        "OWfa3PnctzOOuZz0N"
+      )
+      .then(() => {
+        alert("Message sent successfully!");
+        setText("");
+        setEmail("");
+        setMessage("");
+      })
+      .catch((error) => {
+        console.error(error);
+        alert("Failed to send message");
+      });
+  };
 
   return (
     <div className="relative w-full h-screen overflow-hidden">
       <section
         id="contact"
-        className=" min-h-screen flex items-center   justify-center px-6 py-20"
+        className="min-h-screen flex items-center justify-center px-6 py-20"
       >
-        {/* Form */}
         <form
-          onSubmit={(e) => {
-            submitHandle(e);
-          }}
+          onSubmit={submitHandle}
           className={`space-y-2 text-center ${
-            isDark ? "bg-white" : "bg-white"
+            isDark ? "bg-white border-white/10" : "bg-white"
           } rounded-2xl shadow-2xl p-8`}
         >
           <H1
@@ -40,14 +63,14 @@ const Contact = () => {
             isDark={isDark}
             stagger={0.2}
           />
-          <p className=" text-center text-black mb-8">
+
+          <p className="text-center text-black mb-8">
             Let’s build something amazing together.
           </p>
+
           <input
             value={text}
-            onChange={(e) => {
-              setText(e.target.value);
-            }}
+            onChange={(e) => setText(e.target.value)}
             type="text"
             placeholder="Your Name"
             className="w-full px-4 py-3 rounded-lg bg-gray-400 border border-gray-400 text-black focus:outline-none focus:border-white transition"
@@ -55,9 +78,7 @@ const Contact = () => {
 
           <input
             value={email}
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
+            onChange={(e) => setEmail(e.target.value)}
             type="email"
             placeholder="Your Email"
             className="w-full px-4 py-3 rounded-lg bg-gray-400 border border-gray-400 text-black focus:outline-none focus:border-white transition"
@@ -65,12 +86,10 @@ const Contact = () => {
 
           <textarea
             value={message}
-            onChange={(e) => {
-              setMessage(e.target.value);
-            }}
+            onChange={(e) => setMessage(e.target.value)}
             rows="4"
             placeholder="Your Message"
-            className="w-full px-4 py-3 rounded-lg bg-gray-400 border border-gray-400 text-black focus:outline-none focus:border-white transition "
+            className="w-full px-4 py-3 rounded-lg bg-gray-400 border border-gray-400 text-black focus:outline-none focus:border-white transition"
           ></textarea>
 
           <button
